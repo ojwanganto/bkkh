0520ccf33b613fd5f14255a5ac7d260c4cc267ae
ojwanganto <ojwangwachiaje@gmail.com>
2016-08-09 00:29:09 +0300

    added initial queries

---
+++ bkkh_queries.sql
@@ -0,0 +1,368 @@
+
+-- ====================================================================================================
+-- 												BKKH Project Queries
+-- ====================================================================================================
+
+-- ============================== Patient Daily List Query =============================================
+
+
+select 
+e.patient_id,
+e.encounter_id,
+e.visit_id,
+v.date_stopped,
+date(e.encounter_datetime) as doa,
+pi.identifier as KH_Number,
+concat_ws(' ', pn.family_name, pn.middle_name, pn.given_name) as patient_name,
+datediff(e.encounter_datetime, p.birthdate) div 365.25 as age,
+adm.Primary as 'primary',
+adm.Secondary as 'secondary',
+date(bk.Appointment_Date) as DoO,
+bk.Surgeon as Surgeon,
+bk.Operation as operation,
+dc.date_of_discharge,
+e_notes.notes as notes,
+datediff(curdate(), min(date(bk.Appointment_Date))) as num_days 
+from encounter e 
+inner join visit v on v.visit_id = e.visit_id and v.voided=0
+inner join encounter_type et on et.encounter_type_id=e.encounter_type and et.uuid= 'd02513f0-8b7a-4040-9dbf-7a1eb62cc271'
+inner join person p on p.person_id = e.patient_id and p.voided=0 and p.dead=0
+inner join person_name pn on pn.person_id = p.person_id and pn.voided=0
+left outer join patient_identifier pi on pi.patient_id = e.patient_id and pi.identifier_type=4 and pi.voided=0
+left outer join 
+(
+select 
+person_id, 
+encounter_id,
+group_concat( 
+distinct case 
+when find_in_set('Primary',y.diag)=1 then substring_index(y.diag,',',-1)
+when find_in_set('Primary',y.diag)=2 then substring_index(y.diag,',',1)
+else null end separator ', ') as 'Primary', 
+group_concat(
+distinct case 
+when find_in_set('Secondary',y.diag)=1 then substring_index(y.diag,',',-1)
+when find_in_set('Secondary',y.diag)=2 then substring_index(y.diag,',',1)
+else null end separator ', ') as 'Secondary' 
+from (
+select person_id, encounter_id, group_concat(name) as diag from (
+select o.obs_id,
+person_id,
+o.concept_id,
+o.encounter_id,
+o.order_id,
+o.obs_group_id,
+o.value_coded, 
+cn.name 
+from obs o
+inner join encounter e on e.encounter_id = o.encounter_id and e.voided=0
+inner join encounter_type et on et.encounter_type_id=e.encounter_type and et.uuid= 'd02513f0-8b7a-4040-9dbf-7a1eb62cc271' 
+left outer join concept_name cn on cn.concept_id=o.value_coded and cn.voided=0 and cn.concept_name_type='FULLY_SPECIFIED' and cn.locale='en' 
+where o.obs_group_id is not null and o.concept_id != 159394
+order by o.obs_id desc
+) x
+group by x.obs_group_id
+) y
+group by y.encounter_id
+
+) adm on adm.encounter_id = e.encounter_id 
+left outer join
+(
+select 
+o.encounter_id, 
+o.value_text as notes 
+from obs o 
+where o.concept_id=162169 and o.voided=0
+group by o.encounter_id
+) e_notes on e_notes.encounter_id=e.encounter_id
+left outer join 
+(
+select
+e.encounter_id, 
+patient_id,
+concat(p.given_name,' ',p.family_name) as Surgeon,
+e.encounter_datetime,
+max(if(o.concept_id=5096, o.value_datetime, '')) as Appointment_Date,
+max(if(o.concept_id=1651,cn.name , '')) as Operation,
+e.creator
+from obs o
+inner join encounter e on e.encounter_id=o.encounter_id and e.voided=0
+inner join person_name p on e.creator= p.person_id
+left outer join concept_name cn on cn.concept_id=o.value_coded and cn.voided=0 and cn.concept_name_type='FULLY_SPECIFIED' and cn.locale='en'
+where o.concept_id in (5096, 1651) and e.encounter_type=14
+group by e.encounter_id
+) bk on bk.patient_id = e.patient_id and bk.Appointment_Date between date_add(e.encounter_datetime, interval -2 DAY) and date_add(e.encounter_datetime, interval 2 DAY)
+left outer join
+(
+select 
+patient_id, 
+visit_id, 
+date(encounter_datetime) as date_of_discharge 
+from encounter 
+where encounter_type=9
+group by visit_id
+) dc on dc.patient_id=e.patient_id and dc.visit_id = e.visit_id
+where e.visit_id is not null and (v.date_stopped is null or dc.date_of_discharge is null) and date(e.encounter_datetime) <= :effectiveDate
+group by e.patient_id, e.visit_id
+
+
+-- ===============================================================================================================================
+
+-- ------------------------------------------------------ BKKH Financial Report --------------------------------------------------
+
+
+-- ========================================================== general use =======================================
+
+select 
+pi.identifier as kh_number,
+pn.family_name last_name,
+pn.given_name first_name,
+p.birthdate as dob,
+p.gender as sex,
+bk.Operation,
+bk.Surgeon,
+bk.Appointment_Date as DoO,
+dc.date_of_discharge,
+cost.*
+from
+(
+select 
+c.patient_id,
+adm.Primary,
+adm.Secondary,
+adm.encounter_datetime as DoA,
+adm.location,
+adm.visit_id,
+c.stay_cost,
+c.procedure_cost,
+c. anaesthesia_cost,
+c.date_created as date_of_charge,
+c.doctor_cost,
+c.meds_cost,
+c.lab_cost,
+c.xray_cost,
+c.supplies_cost,
+c.file_cost,
+c.follow_up_cost,  
+a.account_name,
+pc.*
+from bkkh_charges c 
+left outer join (
+select
+max(charges_id) as charges_id,
+max(date(payment_date)) as payment_date,
+charge_account_id,
+max(if(mode_of_payment_id=1, amount_paid, '')) as NHIF,
+max(if(mode_of_payment_id=2, amount_paid, '')) as Individual,
+max(if(mode_of_payment_id=3, amount_paid, '')) as 'Government Sponsored',
+max(if(mode_of_payment_id=4, amount_paid, '')) as 'NGO Sponsored',
+max(if(mode_of_payment_id=5, amount_paid, '')) as 'Insurance',
+max(if(mode_of_payment_id=6, amount_paid, '')) as 'Needy Fund',
+sum(amount_paid) as Total_Paid
+from bkkh_payment
+group by charges_id
+) pc  on c.charges_id = pc.charges_id
+inner join 
+(
+select 
+person_id, 
+visit_id,
+encounter_datetime,
+location, 
+encounter_id,
+group_concat( 
+distinct case 
+when find_in_set('Primary',y.diag)=1 then substring_index(y.diag,',',-1)
+when find_in_set('Primary',y.diag)=2 then substring_index(y.diag,',',1)
+else null end separator ', ') as 'Primary', 
+group_concat(
+distinct case 
+when find_in_set('Secondary',y.diag)=1 then substring_index(y.diag,',',-1)
+when find_in_set('Secondary',y.diag)=2 then substring_index(y.diag,',',1)
+else null end separator ', ') as 'Secondary' 
+from (
+select person_id, visit_id,encounter_id, encounter_datetime, location, group_concat(name) as diag from (
+select o.obs_id,
+person_id,
+o.concept_id,
+o.encounter_id,
+e.encounter_datetime,
+l.name as location,
+e.visit_id,
+o.order_id,
+o.obs_group_id,
+o.value_coded, 
+cn.name 
+from obs o
+inner join encounter e on e.encounter_id = o.encounter_id and e.voided=0
+inner join location l on l.location_id=e.location_id
+inner join encounter_type et on et.encounter_type_id=e.encounter_type and et.uuid= 'd02513f0-8b7a-4040-9dbf-7a1eb62cc271' 
+left outer join concept_name cn on cn.concept_id=o.value_coded and cn.voided=0 and cn.concept_name_type='FULLY_SPECIFIED' and cn.locale='en' 
+where o.obs_group_id is not null and o.concept_id != 159394
+order by o.obs_id desc
+) x
+group by x.obs_group_id
+) y
+group by y.encounter_id
+) adm on adm.visit_id = c.visit_id
+inner join bkkh_charge_account a on a.charge_account_id = pc.charge_account_id
+) cost
+left outer join
+(
+select
+e.encounter_id, 
+patient_id,
+concat(p.given_name,' ',p.family_name) as Surgeon,
+e.encounter_datetime,
+max(if(o.concept_id=5096, o.value_datetime, '')) as Appointment_Date,
+max(if(o.concept_id=1651,cn.name , '')) as Operation,
+e.creator
+from obs o
+inner join encounter e on e.encounter_id=o.encounter_id and e.voided=0
+inner join person_name p on e.creator= p.person_id
+left outer join concept_name cn on cn.concept_id=o.value_coded and cn.voided=0 and cn.concept_name_type='FULLY_SPECIFIED' and cn.locale='en'
+where o.concept_id in (5096, 1651) and e.encounter_type=14
+group by e.encounter_id
+) bk on bk.patient_id = cost.patient_id and bk.Appointment_Date between date_add(cost.DoA, interval -2 DAY) and date_add(cost.DoA, interval 2 DAY)
+left outer join
+(
+select 
+patient_id, 
+visit_id, 
+date(encounter_datetime) as date_of_discharge 
+from encounter 
+where encounter_type=9
+) dc on dc.patient_id=cost.patient_id and dc.visit_id = cost.visit_id
+inner join person p on p.person_id=cost.patient_id and p.voided=0
+inner join person_name pn on pn.person_id = p.person_id and pn.voided=0
+left outer join patient_identifier pi on pi.patient_id = cost.patient_id and pi.identifier_type=4 and pi.voided=0
+having date_of_charge between :startDate and :endDate
+;
+
+
+-- ----------------------------------------------------------- Brief Operative Report -----------------------------------------------------
+
+select
+e.patient_id as patient,
+pi.identifier as khNo,
+concat_ws(' ', pn.family_name, pn.given_name) name,
+p.birthdate as dob,
+datediff(e.encounter_datetime, p.birthdate) div 365.25 as age,
+p.gender as sex,
+date(e.encounter_datetime) as DoO,
+e.encounter_id,
+max(if(o.concept_id = 1651, cn.name,'')) as operation,
+max(if(o.concept_id = 163034, cn.name, '')) as dx1,
+max(if(o.concept_id = 163035, cn.name, '')) as dx2,
+max(if(o.concept_id = 1473, o.value_text, '')) as surgeon,
+max(if(o.concept_id = 160029, cn.name, '')) as findings,
+max(if(o.concept_id = 160632, o.value_text, '')) as operativeDetails,
+max(if(o.concept_id = 5089, o.value_numeric, '')) as weight,
+max(if(o.concept_id = 161928, o.value_numeric, '')) as ebl,
+max(if(o.concept_id = 161929, o.value_numeric, '')) as urine
+from obs o 
+inner join encounter e on e.encounter_id = o.encounter_id and e.voided =0 
+inner join person p on p.person_id=e.patient_id and p.voided=0
+inner join person_name pn on pn.person_id = p.person_id and pn.voided=0
+left outer join patient_identifier pi on pi.patient_id = e.patient_id and pi.identifier_type=4 and pi.voided=0
+left outer join concept_name cn on cn.concept_id=o.value_coded and cn.voided=0 and cn.concept_name_type='FULLY_SPECIFIED' and cn.locale='en'
+where e.encounter_type=16 
+group by e.patient_id, e.encounter_id
+having DoO = :date_of_operation and patient = :Patient
+
+
+-- ===================================================== Report Descriptor ==========================================================
+
+    {
+        "id": "bkkh.reportingui.reports",
+        "description": "BKKH Reports",
+        "order": 10,
+        "extensionPoints": [
+            {
+                "id": "org.openmrs.module.reportingui.reports.overview",
+                "description": "Links to available Overview Reports",
+                "supportedExtensionTypes": [ "link" ]
+            },
+            {
+                "id": "org.openmrs.module.reportingui.reports.dataquality",
+                "description": "Links to available Data Quality Reports",
+                "supportedExtensionTypes": [ "link" ]
+            },
+            {
+                "id": "org.openmrs.module.reportingui.reports.dataexport",
+                "description": "Links to available Data Exports",
+                "supportedExtensionTypes": [ "link" ]
+            }
+        ],
+        "extensions": [
+            {
+                "id": "reportingui.reports.homepagelink",
+                "extensionPointId": "org.openmrs.referenceapplication.homepageLink",
+                "type": "link",
+                "label": "reportingui.reportsapp.home.title",
+                "url": "reportingui/reportsapp/home.page",
+                "icon": "icon-list-alt",
+                "order": 5,
+                "requiredPrivilege": "App: reportingui.reports"
+            },
+            {
+                "id": "reportingui.dataExports.adHoc",
+                "extensionPointId": "org.openmrs.module.reportingui.reports.dataexport",
+                "type": "link",
+                "label": "reportingui.adHocAnalysis.label",
+                "url": "reportingui/adHocManage.page",
+                "order": 9999,
+                "requiredPrivilege": "App: reportingui.adHocAnalysis",
+                "featureToggle": "reportingui_adHocAnalysis"
+            },
+            {
+                "id": "Patient Daily List Report",
+                "extensionPointId": "org.openmrs.module.reportingui.reports.overview",
+                "type": "link",
+                "label": "Patient Daily List Report",
+                "url": "reportingui/runReport.page?reportDefinition=3294a490-7fe0-4341-a270-1438603e41d3",
+                "order": 1,
+                "requiredPrivilege": "App: reportingui.reports"
+            },
+           {
+                "id": "BRRI Financial Report",
+                "extensionPointId": "org.openmrs.module.reportingui.reports.overview",
+                "type": "link",
+                "label": "BRRI Financial Report",
+                "url": "reportingui/runReport.page?reportDefinition=f6aa5cdc-a48b-4e7c-84ac-7a43a0aebdba",
+                "order": 1,
+                "requiredPrivilege": "App: reportingui.reports"
+            },
+            {
+                "id": "Brief Operative Report",
+                "extensionPointId": "org.openmrs.module.reportingui.reports.overview",
+                "type": "link",
+                "label": "Brief Operative Report",
+                "url": "reportingui/runReport.page?reportDefinition=762b7e66-bed2-44ea-982f-48ce4fc40935",
+                "order": 1,
+                "requiredPrivilege": "App: reportingui.reports"
+            }
+        ],
+        "requiredPrivilege": "App: reportingui.reports"
+    }
+
+
+URLs for editing report definitions
+Brief Operative Notes
+- http://41.89.94.14:8080/openmrs/module/reporting/datasets/sqlDataSetEditor.form?uuid=65249821-b18a-429a-bf65-896fdc477402
+
+BRRI Financial Report
+- http://41.89.94.14:8080/openmrs/module/reporting/datasets/sqlDataSetEditor.form?uuid=df0748a7-13f4-4851-a353-c9d2a234b01f
+
+Patient Daily List
+- http://41.89.94.14:8080/openmrs/module/reporting/datasets/sqlDataSetEditor.form?uuid=f4c40476-02e0-437e-bc9f-830f4476c117
+
+
+
+
+Financial Report Design : f6aa5cdc-a48b-4e7c-84ac-7a43a0aebdba
+
+reporting/reports/reportEditor.form?uuid=f6aa5cdc-a48b-4e7c-84ac-7a43a0aebdba
+
+
+					
